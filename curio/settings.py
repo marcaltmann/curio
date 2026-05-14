7643121ab0791a4e3b8967f1aa6bd486b8919865
Marc Altmann <marcaltmann@posteo.de>
2026-05-14 16:58:27 +0200

    Initial commit

---
+++ curio/settings.py
@@ -0,0 +1,150 @@
+from pathlib import Path
+
+from django.utils.translation import gettext_lazy as _
+
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-2w+=02b$r^(@ea05$bwe!wj7*2_orpfjld1c7n5q-gx1!2veic'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DJANGO_ENV = 'development'
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'allauth.account',
+    'allauth',
+    'curio.core',
+    'curio.my_account',
+    'django_vite',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.messages',
+    'django.contrib.sessions',
+    'django.contrib.staticfiles',
+]
+
+if DJANGO_ENV == 'development':
+    INSTALLED_APPS += [
+        'debug_toolbar',
+    ]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+if DJANGO_ENV == 'development':
+    MIDDLEWARE.insert(0, 'debug_toolbar.middleware.DebugToolbarMiddleware')
+
+
+ROOT_URLCONF = 'curio.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'curio.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+# Authentication
+
+AUTH_USER_MODEL = 'my_account.User'
+LOGIN_URL = 'account_login'
+LOGIN_REDIRECT_URL = 'welcome'
+LOGOUT_REDIRECT_URL = 'welcome'
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+ACCOUNT_SIGNUP_FIELDS = ['username*', 'email*', 'password1*', 'password2*']
+ACCOUNT_LOGIN_METHODS = ['username', 'email']
+ACCOUNT_CHANGE_EMAIL = True
+ACCOUNT_SIGNUP_FORM_HONEYPOT_FIELD = 'address'
+ACCOUNT_EMAIL_SUBJECT_PREFIX = '[Curio] '
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+
+
+# Password validation
+# https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/6.0/topics/i18n/
+
+USE_I18N = True
+LANGUAGES = [
+    ('de', _('German')),
+    ('en', _('English')),
+]
+LANGUAGE_CODE = 'en'
+LOCALE_PATHS = (BASE_DIR / 'locale',)
+TIME_ZONE = 'Europe/Berlin'
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/6.0/howto/static-files/
+
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'build'
+STATICFILES_DIRS = [BASE_DIR / 'static', BASE_DIR / 'vite_assets_dist']
---
+++ curio/urls.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from django.contrib import admin
+from django.urls import include, path
+
+import curio.core.views as core_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path('', core_views.welcome, name='welcome'),
+]
+
+if settings.DJANGO_ENV == 'development':
+    urlpatterns.append(path('__debug__/', include('debug_toolbar.urls')))
