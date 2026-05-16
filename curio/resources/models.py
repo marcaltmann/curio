e4e1c2a5f70d9aa7f32966d32ad69e4a5d337e04
Marc Altmann <marcaltmann@posteo.de>
2026-05-16 20:10:43 +0200

    At produced_at field for Resource

--- curio/resources/models.py
+++ curio/resources/models.py
@@ -5,6 +5,9 @@ from django.utils.translation import gettext_lazy as _
 class Resource(models.Model):
     title = models.CharField(max_length=255, verbose_name=_('title'))
     file_size = models.PositiveIntegerField(default=0, verbose_name=_('file size'))
+    produced_at = models.DateTimeField(
+        null=True, blank=True, verbose_name=_('produced at')
+    )
     created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('created at'))
     updated_at = models.DateTimeField(auto_now=True, verbose_name=_('updated at'))
 
